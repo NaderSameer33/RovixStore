95136e3307cf1ef2f774f473a6029f128b5ef9d5
nader <nadersameer@std.mans.edu.eg>
2026-05-17 14:54:49 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:rovix/roviex_store_app.dart';
+
+void main() {
+  runApp(const RovixStoreApp());
+}
---
+++ lib/roviex_store_app.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class RovixStoreApp extends StatelessWidget {
+  const RovixStoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
