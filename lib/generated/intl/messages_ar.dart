b325e680d3eecbac238347570c4c36d2f63b1fa4
nader <nadersameer@std.mans.edu.eg>
2026-05-20 01:35:43 +0300

    Adding a json file for loclization

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -22,6 +22,64 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-    "app_name": MessageLookupByLibrary.simpleMessage("متجر نادر "),
+    "app_name": MessageLookupByLibrary.simpleMessage("متجر نادر"),
+    "application_features": MessageLookupByLibrary.simpleMessage(
+      "مميزات التطبيق",
+    ),
+    "build_developer": MessageLookupByLibrary.simpleMessage("مطور التطبيق"),
+    "build_version": MessageLookupByLibrary.simpleMessage("نسخة التطبيق"),
+    "cancel": MessageLookupByLibrary.simpleMessage("إلغاء"),
+    "change_to_the_language": MessageLookupByLibrary.simpleMessage(
+      "هل تريد تأكيد تغيير اللغة إلى اللغة الإنجليزية؟",
+    ),
+    "choose_products": MessageLookupByLibrary.simpleMessage(
+      "اختر منتجاتك آلان",
+    ),
+    "create_account": MessageLookupByLibrary.simpleMessage("إنشاء حساب"),
+    "dark_mode": MessageLookupByLibrary.simpleMessage("Dark Mode"),
+    "full_name": MessageLookupByLibrary.simpleMessage("الاسم الكامل"),
+    "image_removed": MessageLookupByLibrary.simpleMessage("تم مسح الصورة"),
+    "image_uploaded": MessageLookupByLibrary.simpleMessage(
+      "تم تحميل الصورة بنجاح",
+    ),
+    "lang_code": MessageLookupByLibrary.simpleMessage("العربية"),
+    "language": MessageLookupByLibrary.simpleMessage("English"),
+    "language_tilte": MessageLookupByLibrary.simpleMessage("اللغة"),
+    "log_out": MessageLookupByLibrary.simpleMessage("تسجيل خروج"),
+    "log_out_from_app": MessageLookupByLibrary.simpleMessage(
+      "هل تريد تسجيل الخروج؟",
+    ),
+    "logged_error": MessageLookupByLibrary.simpleMessage(
+      "هناك خطأ في تسجيل الدخول اعد المحاولة",
+    ),
+    "logged_successfully": MessageLookupByLibrary.simpleMessage(
+      "تم تسجيل الدخول بنجاح",
+    ),
+    "login": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
+    "no": MessageLookupByLibrary.simpleMessage("لا"),
+    "password": MessageLookupByLibrary.simpleMessage("كلمة المرور"),
+    "sign_up": MessageLookupByLibrary.simpleMessage("آنشاء حساب"),
+    "sign_up_welcome": MessageLookupByLibrary.simpleMessage(
+      "مرحبا، الرجاء إضافة المعلومات الخاصة بك",
+    ),
+    "sure": MessageLookupByLibrary.simpleMessage("تآكيد"),
+    "valid_email": MessageLookupByLibrary.simpleMessage(
+      "من فضلك إدخال البريد الإلكتروني الصحيح",
+    ),
+    "valid_name": MessageLookupByLibrary.simpleMessage(
+      "من فضلك إدخال اسمك كاملا",
+    ),
+    "valid_passwrod": MessageLookupByLibrary.simpleMessage(
+      "من فضلك إدخال كلمة مرور صحيحة، لا تقل عن 6 أحرف",
+    ),
+    "valid_pick_image": MessageLookupByLibrary.simpleMessage(
+      "الرجاء تحديد الصورة الرمزية الخاصة بك",
+    ),
+    "welcome": MessageLookupByLibrary.simpleMessage(
+      "مرحباً! الرجاء إدخال بريدك إلكتروني والبدء.",
+    ),
+    "yes": MessageLookupByLibrary.simpleMessage("نعم"),
+    "you_have_account": MessageLookupByLibrary.simpleMessage("لديك حساب "),
+    "your_email": MessageLookupByLibrary.simpleMessage("بريدك الالكتروني"),
   };
 }
